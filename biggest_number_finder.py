749d098ff0f7d52ae76c402c18f3ada9918f2408
unknown <jaredtiu1706@gmail.com>
2024-10-17 22:38:31 +0800

    added if statements for numbers 2,3,4 and 5

--- biggest_number_finder.py
+++ biggest_number_finder.py
@@ -5,14 +5,25 @@ def highest_number (number_1, number_2, number_3, number_4, number_5):
         if number_1 > number_3: 
             if number_1 > number_4: 
                 if number_1 > number_5: 
-                    print(f"The highest number is:", number_1) 
+                    print("The highest number is:", number_1) 
 #if it is not the highest then move on to number 2 
     #compare number 2 to number 3, 4, and 5 
+    elif number_2 > number_3: 
+        if number_2 > number_4: 
+            if number_2 > number_5: 
+                print("The highest number is:", number_2)
 #if it is still not the highest move on to number 3
     #compare number 3 to number 4, and 5
+    elif number_3 > number_4: 
+        if number_3 > number_5:
+            print("The highest number is:", number_3)
 #if it not the highest move on to number 4
     #compare number 4 to number 5
+    elif number_4 > number_5: 
+            print("The highest number is:", number_4)
 #if it still not the highest then number 5 is the highest.
+    else:
+        print ("The highest number is:", number_5)
 #asking for user inputs
 number_1 = int(input("input first number: "))
 number_2 = int(input("input second number: "))
