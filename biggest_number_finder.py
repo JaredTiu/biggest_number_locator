1750a505bc8560139153051999a00e19a93a00e7
unknown <jaredtiu1706@gmail.com>
2024-10-17 22:06:58 +0800

    added pseudocode

---
+++ biggest_number_finder.py
@@ -0,0 +1,10 @@
+#asking for user inputs
+# compare the 5 inputs given by the user 
+    #compare number 1 to number 2, 3, 4 and 5 
+#if it is not the highest then move on to number 2 
+    #compare number 2 to number 3, 4, and 5 
+#if it is still not the highest move on to number 3
+    #compare number 3 to number 4, and 5
+#if it not the highest move on to number 4
+    #compare number 4 to number 5
+#if it still not the highest then number 5 is the highest.
