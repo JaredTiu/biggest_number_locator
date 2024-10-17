1a1a8516942be0496161214b7c20db348a70bce9
unknown <jaredtiu1706@gmail.com>
2024-10-17 22:27:39 +0800

    added a definition to input the comparation of the numbers

--- biggest_number_finder.py
+++ biggest_number_finder.py
@@ -1,9 +1,4 @@
-#asking for user inputs
-number_1 = float(input("input first number: "))
-number_2 = float(input("input second number: "))
-number_3 = float(input("input third number: "))
-number_4 = float(input("input fourth number: "))
-number_5 = float(input("input fifth number: ")) 
+def highest_number (number_1, number_2, number_3, number_4, number_5):
 # compare the 5 inputs given by the user 
     #compare number 1 to number 2, 3, 4 and 5 
 #if it is not the highest then move on to number 2 
@@ -13,3 +8,9 @@ number_5 = float(input("input fifth number: "))
 #if it not the highest move on to number 4
     #compare number 4 to number 5
 #if it still not the highest then number 5 is the highest.
+#asking for user inputs
+number_1 = int(input("input first number: "))
+number_2 = int(input("input second number: "))
+number_3 = int(input("input third number: "))
+number_4 = int(input("input fourth number: "))
+number_5 = int(input("input fifth number: ")) 
