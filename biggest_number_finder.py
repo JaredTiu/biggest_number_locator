629e08bb6e647beb98f4eb6f75cc10a9fb6112cb
unknown <jaredtiu1706@gmail.com>
2024-10-17 22:17:11 +0800

    added variables that asks users to input the numbers

--- biggest_number_finder.py
+++ biggest_number_finder.py
@@ -1,4 +1,9 @@
 #asking for user inputs
+number_1 = float(input("input first number: "))
+number_2 = float(input("input second number: "))
+number_3 = float(input("input third number: "))
+number_4 = float(input("input fourth number: "))
+number_5 = float(input("input fifth number: ")) 
 # compare the 5 inputs given by the user 
     #compare number 1 to number 2, 3, 4 and 5 
 #if it is not the highest then move on to number 2 
